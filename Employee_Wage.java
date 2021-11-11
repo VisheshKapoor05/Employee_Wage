686637163ed0d96813ba213d6787165da2851fae
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-11 16:48:55 +0530

    added part time

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -2,16 +2,27 @@ package Employee_Wage;
 
 public class Employee_Wage {
 	public static void main(String[] args) {
-		int present = 1 ;
-	    double wage;
-            double EmpStatus = Math.floor(Math.random()*10)%2;
-            if ( EmpStatus == present )
-               {
-		 wage = (20*EmpStatus)*8 ;
-                 System.out.println("Employee is present and Today's Wage is " +wage);
-               }
-            else
-                System.out.println("Employee is absent and Today's wage is 0");
+
+		int fulltimehours = 8;
+        int fulltime = 2 ;
+
+        int partime = 1 ;
+        int partimehours = 4 ;
+
+        double wage;
+        double EmpStatus = Math.floor(Math.random()*10)%3;
+        if ( EmpStatus == fulltime )
+           {
+             wage = (20*EmpStatus)*fulltimehours ;
+             System.out.println("Employee is doing fulltime and Today's Wage is " +wage);
+           }
+        else if ( EmpStatus == partime )
+        {
+        	wage = (20*EmpStatus)*partimehours ;
+             System.out.println("Employee is doing part-time and the wage is " +wage);
+        }
+        else
+            System.out.println("Employee is doing absent and so wage is 0");
 	}
 
 }
