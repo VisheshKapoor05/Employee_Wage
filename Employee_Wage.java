4ccf459e031e8a44eabb8f3c36d9e603f51ad29a
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-11 16:45:05 +0530

    Employee's presence is checked

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -2,23 +2,12 @@ package Employee_Wage;
 
 public class Employee_Wage {
 	public static void main(String[] args) {
-		final int fulltime = 2 ;      
-        final int partime = 1 ;        
-        int Emphrs;
-        double wage;
-        double EmpStatus = Math.floor(Math.random()*10)%3;
-        	switch ((int) EmpStatus) {
-        	case fulltime:
-        		Emphrs=8;
-        		break;
-        	case partime:
-        		Emphrs=4;
-        		break;
-        	default:
-        		Emphrs=0;
-        	}
-        wage = (20*EmpStatus)*Emphrs ;
-        System.out.println("Employee Wage is " +wage);
+		int present = 1 ;
+	    double EmpStatus = Math.floor(Math.random()*10)%2;
+	    if ( EmpStatus == present )
+		System.out.println("Employee is present");
+	    else
+		System.out.println("Employee is absent");
 	}
 
 }
