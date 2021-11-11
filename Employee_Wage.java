70c763ccfcb11626c112e0808f8fb7b0109f7c0b
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-11 16:56:15 +0530

    added monthly wages

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -2,27 +2,34 @@ package Employee_Wage;
 
 public class Employee_Wage {
 	public static void main(String[] args) {
-
-		int fulltimehours = 8;
-        int fulltime = 2 ;
-
-        int partime = 1 ;
-        int partimehours = 4 ;
-
+		
+        final int fulltime = 2 ;
+        final int partime = 1 ;
+        
+        int Emphrs = 0;
         double wage;
-        double EmpStatus = Math.floor(Math.random()*10)%3;
-        if ( EmpStatus == fulltime )
-           {
-             wage = (20*EmpStatus)*fulltimehours ;
-             System.out.println("Employee is doing fulltime and Today's Wage is " +wage);
-           }
-        else if ( EmpStatus == partime )
+        double MonthlyWage = 0;
+        for (int i=0;i<=20;i++)
         {
-        	wage = (20*EmpStatus)*partimehours ;
-             System.out.println("Employee is doing part-time and the wage is " +wage);
+        	double EmpStatus = Math.floor(Math.random()*10)%3;
+        	
+        	switch ((int) EmpStatus) {
+        	case fulltime:
+        		Emphrs=8;
+        		break;
+        	case partime:
+        		Emphrs=4;
+        		break;
+        	default:
+        		Emphrs=0;
+        	}
+            
+        	wage = (20*EmpStatus)*Emphrs ;
+        	MonthlyWage = MonthlyWage + wage ;
         }
-        else
-            System.out.println("Employee is doing absent and so wage is 0");
+   
+        System.out.println("Monthly wages of Employee is " +MonthlyWage);
+	
 	}
 
 }
