65e629fee2fb74ab9136dfbffcd136dda6ce5e4e
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-11 16:46:59 +0530

    added Daily Wage

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -3,11 +3,15 @@ package Employee_Wage;
 public class Employee_Wage {
 	public static void main(String[] args) {
 		int present = 1 ;
-	    double EmpStatus = Math.floor(Math.random()*10)%2;
-	    if ( EmpStatus == present )
-		System.out.println("Employee is present");
-	    else
-		System.out.println("Employee is absent");
+	    double wage;
+            double EmpStatus = Math.floor(Math.random()*10)%2;
+            if ( EmpStatus == present )
+               {
+		 wage = (20*EmpStatus)*8 ;
+                 System.out.println("Employee is present and Today's Wage is " +wage);
+               }
+            else
+                System.out.println("Employee is absent and Today's wage is 0");
 	}
 
 }
