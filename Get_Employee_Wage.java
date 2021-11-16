12ea5aa3a3f2cf4c2215871ef2ecd79d5825208b
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-16 15:19:49 +0530

    UC9: added arraylist to save values of companies including wage

--- Get_Employee_Wage.java
+++ Get_Employee_Wage.java
@@ -1,5 +1,6 @@
 package Employee_Wage;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Get_Employee_Wage {
@@ -12,6 +13,7 @@ public class Get_Employee_Wage {
 		System.out.print("Enter total number of companies: ");
 		int Num_of_companies = sc.nextInt();
 		
+		ArrayList<Employee_Wage> companies_data = new ArrayList<>();
 		for(int i=0; i<Num_of_companies; i++)
 		{
 			System.out.print("Company Name: ");
@@ -23,10 +25,15 @@ public class Get_Employee_Wage {
 			System.out.print("Max Working Days: ");
 			int Max_Working_Days = sc.nextInt();
 			
-			Employee_Wage company1 = new Employee_Wage(Company_Name, Max_Working_Hours, Max_Working_Days);
-			int MonthlyWage = company1.computing_employee_wage();
-			System.out.println("Company:" +Company_Name);
-			System.out.println("Monthly Wages: " +MonthlyWage);
+			Employee_Wage company = new Employee_Wage(Company_Name, Max_Working_Hours, Max_Working_Days);
+			int MonthlyWage = company.computing_employee_wage();
+			companies_data.add(company);
+			
+			System.out.println("Company:" +Company_Name +" and Monthly Wage: " +MonthlyWage);
+		}
+		
+		for(int i=0; i<Num_of_companies; i++) {
+			System.out.println(companies_data.get(i));
 		}
 		
 	}
