d4cebd284737f71b64df6d3b9f6830496233377c
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-16 14:57:48 +0530

    UC8: refactored code to calculate wages for a multiple companies

--- Get_Employee_Wage.java
+++ Get_Employee_Wage.java
@@ -8,21 +8,27 @@ public class Get_Employee_Wage {
 		
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Company Name: ");
-		String Company_Name = sc.next();
 		
-		System.out.print("Max Working hours: ");
-		int Max_Working_Hours = sc.nextInt();
+		System.out.print("Enter total number of companies: ");
+		int Num_of_companies = sc.nextInt();
 		
-		System.out.print("Max Working Days: ");
-		int Max_Working_Days = sc.nextInt();
+		for(int i=0; i<Num_of_companies; i++)
+		{
+			System.out.print("Company Name: ");
+			String Company_Name = sc.next();
+			
+			System.out.print("Max Working hours: ");
+			int Max_Working_Hours = sc.nextInt();
+			
+			System.out.print("Max Working Days: ");
+			int Max_Working_Days = sc.nextInt();
+			
+			Employee_Wage company1 = new Employee_Wage(Company_Name, Max_Working_Hours, Max_Working_Days);
+			int MonthlyWage = company1.computing_employee_wage();
+			System.out.println("Company:" +Company_Name);
+			System.out.println("Monthly Wages: " +MonthlyWage);
+		}
 		
-		Employee_Wage company1 = new Employee_Wage(Company_Name, Max_Working_Hours, Max_Working_Days);
-		int MonthlyWage = company1.computing_employee_wage();
-		System.out.println(Company_Name);
-		System.out.println("Monthly Wages: " +MonthlyWage);
-
 	}
 
 }
